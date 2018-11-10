4cd25566f4ce1e4a3c52f4bc9d8ed4000821218b
kz6fittycent <jimmy.tigert@gmail.com>
2018-11-09 16:34:50 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="whatami",
-    version="1.0.0",
+    version="1.1.0",
     description="You'd better have some thick skin...or a thick skull!",
     long_description=open("README.md").read(),
     license="MIT",
